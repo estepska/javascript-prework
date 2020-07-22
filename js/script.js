b956e8e019fceb6a8b931918f9c876343cd33d7d
estepska <e_domszy@tlen.pl>
2020-07-22 16:54:51 +0200

    Add new function

--- js/script.js
+++ js/script.js
@@ -28,7 +28,7 @@ printMessage('Mój ruch to: ' + computerMove);*/
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 console.log('Gracz wpisał: ' + playerInput);
 
-let playerMove = getMoveName(computerMove);
+let playerMove = getMoveName(randomNumber);
 
 /*if (playerInput == '1'){
     playerMove = 'kamień';
@@ -40,19 +40,17 @@ let playerMove = getMoveName(computerMove);
 printMessage('Twój ruch to: ' + playerMove);*/
 
 function displayResult(argComputerMove, argPlayerMove){
-    if( argComputerMove == 'kamień' && argPlayerMove == 'papier'){
-      printMessage('Ty wygrywasz!');
-      
-    } else if (
-    (argComputerMove == 'kamień' &&  argPlayerMove == 'nożyce') ||
-    (argComputerMove == 'nożyce' &&  argPlayerMove == 'papier') ||
-    (argComputerMove == 'papier' &&  argPlayerMove == 'kamień') ) {
-    printMessage('Wygra komputer');
-    } else (argPlayerMove == 'nieznany ruch') 
-    printMessage('Zły ruch');
+if((argPlayerMove == 'kamień' && argComputerMove == 'nożyce') ||
+  (argPlayerMove == 'papier' && argComputerMove == 'kamień') ||
+  (argPlayerMove == 'nożyce' && argComputerMove == 'papier')){
+  printMessage('Ty wygrywasz');
+  } else if (argPlayerMove == argComputerMove) {
+  printMessage('Remis');
+  } else  {
+    printMessage ('Przegrana')
+  }
+  
+  }
 
-    printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
-    return 'Remis'
-    }
-
-
+displayResult();
+ 
